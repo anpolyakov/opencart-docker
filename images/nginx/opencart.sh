0f19cc3e88c898dc3d012bfe667ce6a0ad3483aa
Aleksamder Polyakov <warious@yandex.ru>
2020-06-07 23:07:14 +0300

    Add structure of the project

---
+++ images/nginx/opencart.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+apt update && apt upgrade -y \
+apt install nano wget unzip -y
+
+rm -rf opencart 
+wget https://github.com/opencart/opencart/releases/download/3.0.3.3/opencart-3.0.3.3.zip 
+unzip opencart-3.0.3.3.zip
+mv upload opencart
+cd opencart
+pwd
+ls -l | head
+mv config-dist.php config.php
+mv ./admin/config-dist.php ./admin/config.php
+cat config.php
+
