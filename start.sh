a8ecbfe347cce4d94035c306413a7d1fdc56787d
Aleksamder Polyakov <warious@yandex.ru>
2020-06-07 23:49:38 +0300

    Add setup file and change nginx configuration

---
+++ start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Welcome to the Opencart Installation" /n
+
+echo "List of images" /
+
+docker images -a
+
+echo "List of containers" /
+
+docker ps -a
+
+echo "Build new images"
+
+docker build -t bitrix/fpm ./images/fpm/ && docker build -t bitrix/nginx ./images/nginx/
+
+echo "Start Docker compose"
+
+docker compose up
